e31216175dee69a30075a6e608f4ea44013fdb15
James Wol <james.wol@petalmail.com>
2026-07-27 02:22:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,16 @@ from auth import verify_password
 
 from services.user_service import create_admin
 
+from pathlib import Path
+
+css_file = Path("assets/style.css")
+
+if css_file.exists():
+    with open(css_file) as f:
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True
+        )
 
 # =====================================
 # DATABASE INITIALIZATION
