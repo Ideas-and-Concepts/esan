840902b6943aace73eeda8ae1f2cee1e5b3c4d98
James Wol <james.wol@petalmail.com>
2026-07-26 21:49:56 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,82 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    DateTime
+)
 
+from datetime import datetime
+
+from database import Base
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    username = Column(
+        String,
+        unique=True,
+        nullable=False
+    )
+
+    full_name = Column(
+        String
+    )
+
+    password_hash = Column(
+        String,
+        nullable=False
+    )
+
+    role = Column(
+        String,
+        default="User"
+    )
+
+    active = Column(
+        Boolean,
+        default=True
+    )
+
+    created_at = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
+
+
+class Supplier(Base):
+
+    __tablename__ = "suppliers"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    name = Column(String)
+
+    phone = Column(String)
+
+
+class Inventory(Base):
+
+    __tablename__ = "inventory"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    product = Column(String)
+
+    quantity = Column(
+        Integer,
+        default=0
+    )
