b1d0a67a8151ded373e74a1792bfd5527a3e9583
James Wol <james.wol@petalmail.com>
2026-07-27 04:48:49 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,38 +1,39 @@
 """
-Esan ERP Database Engine
+Esan ERP Database Connection
 """
 
-import os
-
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 
-from config import DATABASE_URL
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-if DATABASE_URL.startswith("sqlite"):
-    os.makedirs("data", exist_ok=True)
+
+DATABASE_URL = "sqlite:///esan_erp.db"
+
 
 
 engine = create_engine(
+
     DATABASE_URL,
+
     connect_args={
         "check_same_thread": False
     }
-    if DATABASE_URL.startswith("sqlite")
-    else {}
+
 )
+
 
 
 SessionLocal = sessionmaker(
+
     autocommit=False,
+
     autoflush=False,
+
     bind=engine
+
 )
 
 
+
 Base = declarative_base()
-
-
-def get_session():
-    return SessionLocal()
