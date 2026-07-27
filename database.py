0787eaf7e603abafb680e8b92667cc3795a99fff
James Wol <james.wol@petalmail.com>
2026-07-27 07:37:18 +0300

    Update database.py

--- database.py
+++ database.py
@@ -7,6 +7,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///esan_erp.db"  # Must be defined for the rebuild to work
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
 
 DATABASE_URL = "sqlite:///esan_erp.db"
 
