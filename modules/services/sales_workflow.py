5bdeacb62d803e369e2105c748639e4e965f2021
James Wol <james.wol@petalmail.com>
2026-07-27 05:59:08 +0300

    Create modules/services/sales_workflow.py

---
+++ modules/services/sales_workflow.py
@@ -0,0 +1,21 @@
+approve_order()
+
+        ↓
+
+reserve_stock()
+
+        ↓
+
+create_delivery()
+
+        ↓
+
+create_invoice()
+
+        ↓
+
+record_payment()
+
+        ↓
+
+create_finance_entry()
