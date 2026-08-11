38d6e00b586fcf97b2e292f5f9d2d2ef9df66604
James Wol <james.wol@petalmail.com>
2026-08-11 14:57:42 +0300

    Create deliveries.py

---
+++ modules/sales/deliveries.py
@@ -0,0 +1,10 @@
+"""
+Sales – Deliveries page
+"""
+
+import streamlit as st
+
+def deliveries_page():
+    st.title("🚚 Deliveries")
+    st.info("Delivery tracking will be available soon.")
+    st.markdown("You will be able to schedule deliveries and track their status here.")
