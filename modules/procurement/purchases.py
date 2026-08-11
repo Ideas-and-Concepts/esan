d24e18c3bcff7198d3ec32967ad97253c2d75e46
James Wol <james.wol@petalmail.com>
2026-08-11 15:43:01 +0300

    Update purchases.py

--- modules/procurement/purchases.py
+++ modules/procurement/purchases.py
@@ -1,10 +1,4 @@
-"""
-Procurement – Purchase Orders page
-"""
-
-import streamlit as st
-
-def purchases_page():
-    st.title("📋 Purchase Orders")
-    st.info("Full purchase order management will be available soon.")
-    st.markdown("Here you will be able to view, create and track purchase orders.")
+procurement_purchases = safe_import(
+    "modules.procurement.purchases",
+    "purchases_page"
+)
