3ccefd25baf564f79a94f2d815d30a8c0a5930ef
James Wol <james.wol@petalmail.com>
2026-08-11 14:56:04 +0300

    Create purchases.py

---
+++ modules/procurement/purchases.py
@@ -0,0 +1,10 @@
+"""
+Procurement – Purchase Orders page
+"""
+
+import streamlit as st
+
+def purchases_page():
+    st.title("📋 Purchase Orders")
+    st.info("Full purchase order management will be available soon.")
+    st.markdown("Here you will be able to view, create and track purchase orders.")
