79f51ce4a77e5f8cd49afb059b84848c405d75fe
James Wol <james.wol@petalmail.com>
2026-08-11 14:57:02 +0300

    Create inventory.py

---
+++ modules/warehouse/inventory.py
@@ -0,0 +1,10 @@
+"""
+Warehouse – Inventory page
+"""
+
+import streamlit as st
+
+def inventory_page():
+    st.title("📦 Inventory")
+    st.info("Detailed inventory management will be available soon.")
+    st.markdown("You will be able to view stock levels, movements, and locations here.")
