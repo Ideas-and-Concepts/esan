3ce657d290661588db30a67c2939ad9e64a95428
James Wol <james.wol@petalmail.com>
2026-08-11 14:58:19 +0300

    Create invoices.py

---
+++ modules/sales/invoices.py
@@ -0,0 +1,10 @@
+"""
+Sales – Invoices page
+"""
+
+import streamlit as st
+
+def invoices_page():
+    st.title("🧾 Invoices")
+    st.info("Invoice management will be available soon.")
+    st.markdown("You will be able to create and manage invoices here.")
